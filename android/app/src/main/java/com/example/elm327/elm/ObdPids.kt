ff9c3777d6f6da6b05de4762010574cf51c27d91
eva <evaorange73@gmail.com>
2024-08-14 05:47:02 +0300

    some printers and decoders

--- android/app/src/main/java/com/example/elm327/elm/ObdPids.kt
+++ android/app/src/main/java/com/example/elm327/elm/ObdPids.kt
@@ -7,15 +7,16 @@ enum class ObdPids(
     val decoder: Decoders
 ) {
     NO_PID_FOUND("", "", "", Decoders.DEFAULT),
-    PID_01("01", "", "Supported PIDs [01-20]", Decoders.DEFAULT);
+    PID_01("01", "", "Supported PIDs [01-20]", Decoders.DEFAULT),
+    ;
 
     companion object {
-        private val map : Map<String, ObdPids> = entries.associateBy(ObdPids::pid)
+        private val map: Map<String, ObdPids> = entries.associateBy(ObdPids::pid)
         operator fun get(pid: String) = map[pid] ?: NO_PID_FOUND
 
         fun parse(rawData: String): Pair<ObdPids, DecodedValue> {
             val data = rawData.replace("\\s".toRegex(), "")
-            val pidString = data.slice(7 .. 8)
+            val pidString = data.slice(7..8)
             val pid = ObdPids[pidString]
             return Pair(pid, pid.decoder.decode(data))
         }
@@ -23,11 +24,19 @@ enum class ObdPids(
 }
 
 
-
 enum class Decoders(val decode: (String) -> DecodedValue) {
 
     DEFAULT({ value -> RawData(value) }),
-    PERCENT({ value -> SingleValue(Parser(value).A * 100 / 255, Printers.PERCENTAGE) });
+    PERCENT({ value -> SingleValue(Parser(value).A * 100 / 255, Printers.PERCENTAGE) }),
+    PERCENT_CENTERED({ value ->SingleValue((Parser(value).A - 128) * 100 / 128,Printers.PERCENTAGE) }),
+    TEMPERATURE({ value -> SingleValue(Parser(value).A + 233, Printers.TEMPERATURE) }),
+    FUEL_PRESSURE({ value -> SingleValue(Parser(value).A * 3, Printers.TEMPERATURE) }),
+    RPM({ value -> SingleValue(Parser(value).AB / 4, Printers.ENGINE_SPEED) }),
+    TIMING_ADVANCE({ value -> SingleValue(Parser(value).A / 2 - 64, Printers.ANGLE) }),
+    AIR_FLOW_RATE({ value -> SingleValue(Parser(value).AB / 100, Printers.FLOW) }),
+
+    ;
+
 
     class Parser(private var rawData: String) {
         val A = rawData.slice(0..1).toInt(radix = 16).toFloat()
@@ -54,4 +63,16 @@ class PidsList(val pids: List<Boolean>, printer: Printers) : DecodedValue(printe
 enum class Printers(val print: (DecodedValue) -> String) {
     DEFAULT({ value -> (value as RawData).rawData }),
     PERCENTAGE({ value -> "${(value as SingleValue).singleValue} %" }),
+    VOLTAGE({ value -> "${(value as SingleValue).singleValue} V" }),
+    TEMPERATURE({ value -> "${(value as SingleValue).singleValue} K" }),
+    PRESSURE_KPA({ value -> "${(value as SingleValue).singleValue} kPa" }),
+    PRESSURE_PA({ value -> "${(value as SingleValue).singleValue} Pa" }),
+    ENGINE_SPEED({ value -> "${(value as SingleValue).singleValue} rpm" }),
+    SPEED({ value -> "${(value as SingleValue).singleValue} km/h" }),
+    ANGLE({ value -> "${(value as SingleValue).singleValue} °" }),
+    FLOW({ value -> "${(value as SingleValue).singleValue} g/s" }),
+    VOLTAGE_AND_PERCENTAGE({ value -> "${(value as TwoValues).first} V, ${value.second} %" }),
+    TIME({ value -> "${(value as SingleValue).singleValue} sec" }),
+    DISTANCE({ value -> "${(value as SingleValue).singleValue} km" }),
+    RATIO_AND_VOLTAGE({ value -> "${(value as TwoValues).first} (ratio), ${value.second} V" }),
 }
